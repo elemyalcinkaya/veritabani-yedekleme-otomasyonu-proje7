2a0c1efa5713ef11b948358972442423d7b74ac2
elemyalcinkaya <22290419@ogrenci.ankara.edu.tr>
2026-04-18 13:26:23 +0300

    manuel backup ve T-SQL sorguları eklendi

---
+++ log-backup.sql
@@ -0,0 +1,3 @@
+BACKUP LOG Project1
+TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\Project1_log_backup.trn'
+WITH INIT;
---
+++ step_query.sql
@@ -0,0 +1,3 @@
+BACKUP DATABASE Project1
+TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\Project1_auto_backup.bak'
+WITH INIT;
